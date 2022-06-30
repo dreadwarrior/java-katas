f6fdb0780e35914f6ee1f6f790b4d4964e425734
Thomas Juhnke <tommy@van-tomas.de>
2022-06-30 19:25:50 +0200

    Implement other test cases
    Moving of the static SUT ensures having a proper code coverage report in
    IntelliJ.

---
+++ src/main/java/de/dreadlabs/OptionalStreamArrayOperations.java
@@ -0,0 +1,49 @@
+package de.dreadlabs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OptionalStreamArrayOperations {
+
+    private OptionalStreamArrayOperations() {
+    }
+
+    /**
+     * @param configs comma separated list of ids; e.g. "1,2,3"
+     * @return List of ids without empty items; e.g. configs="1, ,,4" will result in return value of "1,4"
+     */
+    static List<String> doIt(String configs) {
+        ArrayList<String> list = new ArrayList<>();
+
+        if (configs != null && !configs.trim().isEmpty()) { // TODO: compare with call to StringValidator.isEmptyString() at line 5
+            // split the list and put all items into the list
+            if (!StringValidator.isEmptyString(configs)) {
+                String[] feld = configs.split(",");
+                if (feld != null) {
+                    if (feld.length > 0) {
+                        for (String dummy : feld) {
+                            String tmp = dummy.trim();
+                            if (tmp.length() > 0) {
+                                list.add(tmp);
+                            }
+                        }
+                    } else { // no comma
+                        list.add(configs);
+                    }
+                }
+            }
+        }
+
+        return list;
+    }
+
+    private static class StringValidator {
+
+        private StringValidator() {
+        }
+
+        public static boolean isEmptyString(String string) {
+            return string == null || string.trim().isEmpty();
+        }
+    }
+}
--- src/test/java/de/dreadlabs/OptionalStreamArrayOperationsTest.java
+++ src/test/java/de/dreadlabs/OptionalStreamArrayOperationsTest.java
@@ -1,9 +1,7 @@
 package de.dreadlabs;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
@@ -15,59 +13,21 @@ class OptionalStreamArrayOperationsTest {
 
     @ParameterizedTest
     @NullSource
-    @ValueSource(strings = {"", " "})
+    @ValueSource(strings = {"", " ", ", ,", ",,,    "})
     void returnsEmptyList(String input) {
-        assertThat(doIt(input)).isEmpty();
+        assertThat(OptionalStreamArrayOperations.doIt(input)).isEmpty();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {",", ",,", ",,,"})
+    void returnsInputAsIsIfOnlyContainingCommas(String input) {
+        // @see https://stackoverflow.com/a/28035974
+        assertThat(OptionalStreamArrayOperations.doIt(input)).isEqualTo(List.of(input));
     }
 
     @Test
-    @Disabled("until I make it")
-    void returnsEmptyListIfNothingNeedsToBeSplitted() {
+    void returnsListWithoutEmptyOrBlankItemsButTrimmedItems() {
+        assertThat(OptionalStreamArrayOperations.doIt("1,, ,2, 3 ,4")).isEqualTo(List.of("1", "2", "3", "4"));
     }
 
-    @Test
-    @Disabled("until I make it")
-    void returnsListWithoutEmptyItems() {
-    }
-
-    @Test
-    @Disabled("until I make it")
-    void returnsListWithoutBlankItems() {
-    }
-
-    /**
-     * @param configs comma separated list of ids; e.g. "1,2,3"
-     * @return List of ids without empty items; e.g. configs="1, ,,4" will result in return value of "1,4"
-     */
-    private List<String> doIt(String configs) {
-        ArrayList<String> list = new ArrayList<>();
-
-        if (configs != null && !configs.trim().isEmpty()) { // TODO: compare with call to StringValidator.isEmptyString() at line 5
-            // split the list and put all items into the list
-            if (!StringValidator.isEmptyString(configs)) {
-                String[] feld = configs.split(",");
-                if (feld != null) {
-                    if (feld.length > 0) {
-                        for (String dummy : feld) {
-                            String tmp = dummy.trim();
-                            if (tmp.length() > 0) {
-                                list.add(tmp);
-                            }
-                        }
-                    } else { // no comma
-                        list.add(configs);
-                    }
-                }
-            }
-        }
-
-        return list;
-    }
-
-    static class StringValidator {
-
-        public static boolean isEmptyString(String string) {
-            return string == null || string.trim().isEmpty();
-        }
-    }
 }
