6d633ef5d8e478f18d2e9f43d45cde4bb07d86bd
Thomas Juhnke <tommy@van-tomas.de>
2022-06-30 19:27:46 +0200

    ref 1/: remove unnecessary call to StringValidator#isEmptyString()

--- src/main/java/de/dreadlabs/OptionalStreamArrayOperations.java
+++ src/main/java/de/dreadlabs/OptionalStreamArrayOperations.java
@@ -15,35 +15,23 @@ public class OptionalStreamArrayOperations {
     static List<String> doIt(String configs) {
         ArrayList<String> list = new ArrayList<>();
 
-        if (configs != null && !configs.trim().isEmpty()) { // TODO: compare with call to StringValidator.isEmptyString() at line 5
+        if (configs != null && !configs.trim().isEmpty()) {
             // split the list and put all items into the list
-            if (!StringValidator.isEmptyString(configs)) {
-                String[] feld = configs.split(",");
-                if (feld != null) {
-                    if (feld.length > 0) {
-                        for (String dummy : feld) {
-                            String tmp = dummy.trim();
-                            if (tmp.length() > 0) {
-                                list.add(tmp);
-                            }
+            String[] feld = configs.split(",");
+            if (feld != null) {
+                if (feld.length > 0) {
+                    for (String dummy : feld) {
+                        String tmp = dummy.trim();
+                        if (tmp.length() > 0) {
+                            list.add(tmp);
                         }
-                    } else { // no comma
-                        list.add(configs);
                     }
+                } else { // no comma
+                    list.add(configs);
                 }
             }
         }
 
         return list;
-    }
-
-    private static class StringValidator {
-
-        private StringValidator() {
-        }
-
-        public static boolean isEmptyString(String string) {
-            return string == null || string.trim().isEmpty();
-        }
     }
 }
