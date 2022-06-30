316bfa00764649fceabee06a1cdcc45b97c55b29
Thomas Juhnke <tommy@van-tomas.de>
2022-06-30 18:19:02 +0200

    Make "configs" a function input parameter, specify return type

--- src/test/java/de/dreadlabs/OptionalStreamArrayOperationsTest.java
+++ src/test/java/de/dreadlabs/OptionalStreamArrayOperationsTest.java
@@ -1,12 +1,17 @@
 package de.dreadlabs;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class OptionalStreamArrayOperationsTest {
 
-    private void doIt() {
+    /**
+     * @param configs comma separated list of ids; e.g. "1,2,3"
+     * @return List of ids without empty items; e.g. configs="1, ,,4" will result in return value of "1,4"
+     */
+    private List<String> doIt(String configs) {
         ArrayList<String> list = new ArrayList<>();
-        String configs = params.getConfigurationIds(); // TODO: CSV list payload
+
         if (configs != null && !configs.trim().isEmpty()) { // TODO: compare with call to StringValidator.isEmptyString() at line 5
             // split the list and put all items into the list
             if (!StringValidator.isEmptyString(configs)) { // TODO: method body like: string == null || string.trim().isEmpty()
@@ -25,5 +30,7 @@ public class OptionalStreamArrayOperationsTest {
                 }
             }
         }
+
+        return list;
     }
 }
