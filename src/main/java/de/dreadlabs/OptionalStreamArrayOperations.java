71e688627da87215f52b0881b9695a981ca39d6f
Thomas Juhnke <tommy@van-tomas.de>
2022-06-30 22:24:42 +0200

    ref 10/: Switch to original output type ArrayList

--- src/main/java/de/dreadlabs/OptionalStreamArrayOperations.java
+++ src/main/java/de/dreadlabs/OptionalStreamArrayOperations.java
@@ -1,8 +1,10 @@
 package de.dreadlabs;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class OptionalStreamArrayOperations {
 
@@ -13,18 +15,18 @@ public class OptionalStreamArrayOperations {
      * @param csvIds comma separated list of ids; e.g. "1,2,3"
      * @return List of ids without empty items; e.g. csvIds="1, ,,4" will result in return value of "1,4"
      */
-    static List<String> convertCsvIdsToList(String csvIds) {
+    static ArrayList<String> convertCsvIdsToList(String csvIds) {
         if (Optional.ofNullable(csvIds).map(String::isBlank).orElse(true)) {
-            return List.of();
+            return new ArrayList<>(List.of());
         }
 
         // split the list and put all items into the list
         String[] ids = csvIds.split(",");
 
         if (ids.length <= 0) { // no comma
-            return List.of(csvIds);
+            return new ArrayList<>(List.of(csvIds));
         }
 
-        return Arrays.stream(ids).map(String::trim).filter(it -> it.length() > 0).toList();
+        return Arrays.stream(ids).map(String::trim).filter(it -> it.length() > 0).collect(Collectors.toCollection(ArrayList::new));
     }
 }
