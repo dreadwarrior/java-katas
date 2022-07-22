b17a29f4f215a9dbd2adfab50b16cf7ebb5ba17b
Thomas Juhnke <thomas.juhnke@spreadgroup.com>
2022-07-22 09:28:32 +0200

    ref 18/: Use early returns and simplify result operation

--- src/main/java/de/dreadlabs/refactoring/optionalstreamarrayoperations/FunctionalConverter.java
+++ src/main/java/de/dreadlabs/refactoring/optionalstreamarrayoperations/FunctionalConverter.java
@@ -3,20 +3,26 @@ package de.dreadlabs.refactoring.optionalstreamarrayoperations;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class FunctionalConverter implements CommaDelimitedStringConverter {
 
     @Override
     public List<String> toList(String input) {
-        return Optional.ofNullable(input)
-                .map(ids -> ids.split(","))
-                .stream()
-                .flatMap(ids -> ids.length <= 0 ? Stream.of(input) : Arrays.stream(ids))
+        if (input == null || input.isBlank()) {
+            return new ArrayList<>();
+        }
+
+        String[] ids = input.split(",");
+        boolean inputConsistsOfCommasOnly = ids.length == 0;
+        if (inputConsistsOfCommasOnly) {
+            return List.of(input);
+        }
+
+        return Arrays.stream(ids)
                 .map(String::trim)
-                .filter(id -> id.length() > 0)
+                .filter(Predicate.not(String::isEmpty))
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 }
